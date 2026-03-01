1735efbe5b964e4019b2e492c7a47bac7f57082a
Dave N <shekelator@gmail.com>
2026-03-01 22:12:24 +0000

    fix: Make logging work correctly

--- main.py
+++ main.py
@@ -400,4 +400,10 @@ async def redirect_url(short_id: str, request: Request):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=settings.port)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=settings.port,
+        log_level="info",
+        access_log=True,
+    )
